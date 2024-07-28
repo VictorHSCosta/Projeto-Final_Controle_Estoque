c9bc552503b47c8cd9dd3c05fc15cddada0ccaeb
victorHScosta <victorhenriquecosta23@gmail.com>
2024-07-27 23:20:48 -0300

    terminado a tela de adicionar livro testada

--- src/app/(gerenciamento)/adicionar/page.tsx
+++ src/app/(gerenciamento)/adicionar/page.tsx
@@ -1,30 +1,13 @@
 import Link from "next/link";
+import { AddLivro } from "~/app/_components/add_livro"; // Import the AddLivro component
 
 import styles from "./index.module.css";
-
-import styles_module from "~/styles/adicionar.module.css";
 
 export default function Home() {
 
   return (
-    <section>
-
-      <h1 className={styles_module.h1}>Adicionar</h1>
-
-      <form className={styles_module.forms}>
-        <label className={styles_module.label}>Titulo</label>
-        <input type="text" className={styles_module.input} />
-        <label className={styles_module.label}>Quantidade</label>
-        <input type="text" className={styles_module.input}/>
-        <label className={styles_module.label}>Preço</label>
-        <img src="public/image ico.svg" alt="" />
-        <input type="number" className={styles_module.input}/>
-        <label className={styles_module.label}>Descrição</label>
-        <input type="text" className={styles_module.descricao}/>
-        <button type="submit" className={styles_module.button}>
-          <p className={styles_module.txt}>Adicionar</p>
-        </button>
-      </form>
-    </section>
+    <main>
+      <AddLivro/>
+    </main>
   );
 }
