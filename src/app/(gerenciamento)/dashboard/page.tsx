8579387cdb8d7fc917b758e137d36f97a844f26c
Victor Malta <147455879+Malta22v@users.noreply.github.com>
2024-07-28 15:58:40 -0300

    Update page.tsx

--- src/app/(gerenciamento)/dashboard/page.tsx
+++ src/app/(gerenciamento)/dashboard/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
-
 import styles from "./index.module.css";
 
 export default function Home() {
   return (
-    <section>pagina dashboard</section>
+    <>
+      <section className={styles.dashboardSection}>Dashboard</section>
+      <div className={styles.dashboard}>
+        <h1>Atividades Recentes
+          <p>1°Harry Potter</p>
+          <p>2° O poder do Hábito</p>
+          <p>3° Diário de um Banana</p>
+        </h1>
+        <h1>
+          Melhores Vendas
+        </h1>
+        <p>1°Harry Potter</p>
+        <p>2° O poder do Hábito</p>
+        <p>3° Diário de um Banana</p>
+
+        <h1>Estoque</h1>
+        <p>Total de livros:220</p>
+
+      </div>
+    </>
   );
 }
